57ef4c4bb05fe21a54e42ef1f4485f445260fa43
Rajesh Ganesan <rganesan@mmm.com>
2018-11-02 00:09:21 -0500

    basic Lambda practice commit

--- src/BasicLambdaExpression.java
+++ src/BasicLambdaExpression.java
@@ -12,8 +12,48 @@ public class BasicLambdaExpression {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		  BasicLambdaExpression tester = new BasicLambdaExpression();
+			
+	      //with type declaration
+	      MathOperation addition = (int a, int b) -> a + b;
+			
+	      //with out type declaration
+	      MathOperation subtraction = (a, b) -> a - b;
+			
+	      //with return statement along with curly braces
+	      MathOperation multiplication = (int a, int b) -> { return a * b; };
+			
+	      //without return statement and without curly braces
+	      MathOperation division = (int a, int b) -> a / b;
+			
+	      System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+	      System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+	      System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+	      System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+			
+	      //without parenthesis
+	      GreetingService greetService1 = message ->
+	      System.out.println("Hello " + message);
+			
+	      //with parenthesis
+	      GreetingService greetService2 = (message) ->
+	      System.out.println("Hello " + message);
+			
+	      greetService1.sayMessage("Rajeshkumar");
+	      greetService2.sayMessage("Ganesan");
 	}
+	
+	interface MathOperation {
+	      int operation(int a, int b);
+	   }
+		
+	   interface GreetingService {
+	      void sayMessage(String message);
+	   }
+		
+	   private int operate(int a, int b, MathOperation mathOperation) {
+	      return mathOperation.operation(a, b);
+	   }
 
 }
